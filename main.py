dd7bed2197e1a635b2914f18c81d0d6bec318f0b
Christopher Tjondro <chris462@students.cs.ubc.ca>
2024-11-01 20:27:04 -0700

    Added memory capabilities, allowing the llm agent to view past prompts and answers

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ st.header("LangChain Udemy Course- Documentation Helper Bot")
 
 prompt = st.text_input("Prompt", placeholder="Enter your prompt here..")
 
-if "user_prompt_history" not in st.session_state:
-    st.session_state["user_prompt_history"] = []
-
-if "chat_answers_history" not in st.session_state:
+if (
+    "chat_answers_history" not in st.session_state
+    and "user_prompt_history" not in st.session_state
+    and "chat_history" not in st.session_state
+):
     st.session_state["chat_answers_history"] = []
+    st.session_state["user_prompt_history"] = []
+    st.session_state["chat_history"] = []
 
 
 def create_sources_string(source_urls: Set[str]) -> str:
@@ -22,24 +25,30 @@ def create_sources_string(source_urls: Set[str]) -> str:
     sources_list = list(source_urls)
     sources_list.sort()
     sources_string = "sources:\n"
-    for i, source in enumerate(sources_list): # just enumerates sources
+    for i, source in enumerate(sources_list):
         sources_string += f"{i+1}. {source}\n"
     return sources_string
 
 
 if prompt:
-    with st.spinner("Generating response.."): 
-        generated_response = run_llm(query=prompt)
+    with st.spinner("Generating response.."):
+        generated_response = run_llm(
+            query=prompt, chat_history=st.session_state["chat_history"]
+        )
+        print(generated_response)
         sources = set(
-            [doc.metadata["source"] for doc in generated_response["source_documents"]]
+            [doc.metadata["source"] for doc in generated_response["context"]]
         )
 
         formatted_response = (
-            f"{generated_response['result']} \n\n {create_sources_string(sources)}"
+            f"{generated_response['answer']} \n\n {create_sources_string(sources)}"
         )
 
         st.session_state["user_prompt_history"].append(prompt)
         st.session_state["chat_answers_history"].append(formatted_response)
+        st.session_state["chat_history"].append(("human", prompt))
+        st.session_state["chat_history"].append(("ai", generated_response["answer"]))
+
 
 if st.session_state["chat_answers_history"]:
     for generated_response, user_query in zip(
